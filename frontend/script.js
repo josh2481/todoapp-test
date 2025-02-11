f09e5147245d7cd96b8ed6bdb5be2eafae3556b9
josh2481 <166363882+josh2481@users.noreply.github.com>
2025-02-11 10:51:40 +0100

    Delete frontend directory

--- frontend/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('login-form')?.addEventListener('submit', function(event) {
-  event.preventDefault();
-  alert('Login clicked');
-});
-
-document.getElementById('signup-form')?.addEventListener('submit', function(event) {
-  event.preventDefault();
-  alert('Signup clicked');
-});
